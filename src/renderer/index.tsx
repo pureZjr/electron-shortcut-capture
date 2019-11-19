ed852a9678ea2299d4894c7043389e0e6a0bfdb6
zhangjunrao <zhangjunrao@qq.com>
2019-11-19 16:58:15 +0800

    feat: 鼠标右键关闭截图

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,9 +1,14 @@
-import ReactDom from "react-dom";
-import React from "react";
+import ReactDom from 'react-dom'
+import React from 'react'
+import { ipcRenderer } from 'electron'
 
-import ScreenShot from "./screenShot";
+import ScreenShot from './screenShot'
+
+window.addEventListener('contextmenu', () => {
+	ipcRenderer.send('ShortcutCapture::HIDE')
+})
 
 const render = () => {
-  ReactDom.render(<ScreenShot />, document.querySelector("#app"));
-};
-render();
+	ReactDom.render(<ScreenShot />, document.querySelector('#app'))
+}
+render()

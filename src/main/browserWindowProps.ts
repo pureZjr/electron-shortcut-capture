5831f4fedebdb6b412cd579edaabb1924aef6bba
zhangjunrao <zhangjunrao@qq.com>
2019-11-23 10:15:55 +0800

    feat: 允许只在当前屏幕截图

--- src/main/browserWindowProps.ts
+++ src/main/browserWindowProps.ts
@@ -13,33 +13,29 @@ const browserWindowProps = (
 		useContentSize: true,
 		frame: false,
 		show: true,
-		autoHideMenuBar: true,
 		transparent:
 			process.platform === 'darwin' || process.platform === 'win32',
 		// 窗口不可以可以改变尺寸
 		resizable: false,
 		// 窗口不可以拖动
 		movable: false,
-		focusable: process.platform === 'win32',
 		// 全屏窗口
-		fullscreen: true,
+		fullscreen: false,
 		// 在 macOS 上使用 pre-Lion 全屏
 		simpleFullscreen: true,
 		backgroundColor: '#30000000',
 		// 隐藏标题栏, 内容充满整个窗口
-		titleBarStyle: 'hidden',
-		// 窗口永远在别的窗口的上面
-		alwaysOnTop:
-			process.env.NODE_ENV === 'production' ||
-			process.platform === 'darwin',
+		titleBarStyle: 'default',
 		// 窗口允许大于屏幕
+		focusable: true,
 		enableLargerThanScreen: true,
 		// 是否在任务栏中显示窗口
-		skipTaskbar: process.env.NODE_ENV === 'production',
+		skipTaskbar: false,
 		// 窗口不可以最小化
 		minimizable: false,
 		// 窗口不可以最大化
 		maximizable: false,
+		hasShadow: false,
 		webPreferences: {
 			//集成Node
 			nodeIntegration: true
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,14 @@
-import { app } from 'electron'
+import { app, globalShortcut } from 'electron'
 import ElectronShortcutCapture from './electron-shortcut-capture'
 
 app.on('ready', () => {
 	require('electron-debug')({ devToolsMode: 'right', showDevTools: true })
-	new ElectronShortcutCapture().show()
+
+	globalShortcut.register('alt+shift+w', () => {
+		new ElectronShortcutCapture({
+			multiScreen: false
+		}).show()
+	})
 })
 
 app.on('window-all-closed', () => {
